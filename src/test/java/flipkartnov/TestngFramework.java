df0ebee8ce8f8ab7d0c08fc0fed7880902adff40
ananth2 <ananthram.p@tresbu.com>
2018-12-04 19:04:21 +0530

    getting all anchor tags

--- src/test/java/flipkartnov/TestngFramework.java
+++ src/test/java/flipkartnov/TestngFramework.java
@@ -1,7 +1,11 @@
 package flipkartnov;
 
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -34,6 +38,9 @@ public class TestngFramework {
 	public void after() {
 
 		System.out.println("after test is printed ");
+		
+	List<WebElement>lk=	dr.findElements(By.tagName("a"));
+	System.out.println(lk.size());
 
 	}
 	
